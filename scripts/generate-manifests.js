def0157cc1d2fc2f63db58b910524065a7bb301c
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-07-26 16:25:22 +0100

    do not use node: prefix

--- scripts/generate-manifests.js
+++ scripts/generate-manifests.js
@@ -1,4 +1,4 @@
-const { execSync } = require("node:child_process");
+const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const archMap = require("./architecture-map.json");
