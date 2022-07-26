c8bd4ee3e93f334d2a76bea37c7db5d9b39ad1b2
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-07-26 16:48:26 +0100

    Stub release script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,9 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../npm
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && npm publish --registry http://localhost:4873/" \;
+
+# TODO debug info, remove
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && cat package.json" \;
+#
+
+# find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && npm publish --registry http://localhost:1/" \;
