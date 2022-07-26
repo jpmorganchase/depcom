9d11244ac3ffc86cf3fc6ac703a8c623896eca9e
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-07-26 15:59:36 +0100

    Add dry build to tests

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../npm
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && npm publish --registry http://localhost:4873/" \;
