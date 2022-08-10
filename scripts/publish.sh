a23aa96b545b14c829c98911d4f94bcc4f5fcb13
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-08-10 11:18:16 +0100

    Add more clear logging

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,8 +4,10 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../npm
 
-# TODO debug info, remove
+# Debug info
+echo "Showing package.json for all the packages we are going to publish"
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && cat package.json" \;
+echo "-----------------------------------------------------------------------------------------------------------------"
 #
 
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && npm publish" \;
